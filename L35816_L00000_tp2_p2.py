4e6238fe3b1fd7cd64b8fae1eba1831800e84f8a
luciadefrancesco <132778429+luciadefrancesco@users.noreply.github.com>
2023-05-14 15:11:59 -0300

    Update L35816_L00000_tp2_p2.py

--- L35816_L00000_tp2_p2.py
+++ L35816_L00000_tp2_p2.py
@@ -1,11 +1,11 @@
-import L35816_L00000_tp2_p1 as part1
+import L35816_L00000_tp2_p1 as part1                # Al ser un ejercicio muy similiar al 1, utilizamos las funciones definidas en ese ejercicio para optimizar lineas de código.
 
 def main():
     print('≡≡Encriptador de Cifrado de Vigenère≡≡')
     data = part1.try_path('Ingrese nombre del archivo encriptado: ')
     code = part1.try_code('Ingrese la clave: ')
     archive = part1.try_archive('Ingrese nombre del archivo para la encripción: ')
-    part1.encrypter(data, code, archive, desencrypter=True)
+    part1.encrypter(data, code, archive, desencrypter=True)                         # El argumento desencrypter decifra el codigo cuando toma el valor true.
 
 if __name__ == '__main__':
     main()
