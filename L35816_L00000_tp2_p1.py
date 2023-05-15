66026811e7d611391e8e913ff7c39e816b6c61a2
tizianodemarco <127427719+tizianodemarco@users.noreply.github.com>
2023-05-14 23:23:37 -0300

    Update L35816_L00000_tp2_p1.py

--- L35816_L00000_tp2_p1.py
+++ L35816_L00000_tp2_p1.py
@@ -7,48 +7,68 @@ def main():
     archive = try_archive('Ingrese nombre del archivo para la encripción: ')
     encrypter(data, code, archive)
 
-def encrypter(data, code, archive:str, desencrypter=False):
-'''
-Cifra un archivo de entrada utilizando el Cifrado de Vigenère y escribe el texto cifrado en un archivo de salida
-Argumentos:
-Data -- Contenido del archivo de entrada
-Code -- Clave de cifrado (str)
-Archive -- Archivo de salida
-Desencrypter -- Indica si los datos se cifran (false) o descifran (true)
-'''
-    dictionary = {}
+def encrypter(data:list, code:str, archive:str, desencrypter=False):      
+    '''
+    Por default, recibe una clave y encripta un archivo de entrada utilizando el Cifrado de Vigenère. Escribe el texto cifrado en 
+    un archivo nuevo cuyo nombre fue elegido por el usuario. La función también desencripta en caso de que 'desencrypter' sea 
+    verdadero. Esto se utilizará para la parte 2.
+    
+    Parámetros de entrada
+    ---------------------
+    data: list
+    Archivo de entrada a encriptar o desencriptar
+    code: str
+    Clave para el cifrado
+    archive: str
+    Nombre para el archivo de salida
+    desencrypter: bool
+    Indica si los datos se cifran (false) o descifran (true). Por default es false.
+    '''
+    dictionary = {}                                     
     inverted_dictionary = {}
     for number, letter in enumerate(ABC):
-        dictionary[letter] = number
+        dictionary[letter] = number                     # Se generan un diccionario cuya key es el caracter y cuyo value el número que le corresponde, y otro a la inversa. Estos datos serán utiles para el cifrado o descifrado
         inverted_dictionary[number] = letter
 
-    with open (archive, 'w') as f:     
-        counter = 0
+    with open (archive, 'w') as f:
+
+        code_counter = 0
         for line in data:
             line = str(line).lower()
             new_line = ''
+
             for chr in line:
                 if chr in ABC:
                     if not desencrypter:
-                        new_ord = (dictionary[chr]+dictionary[code[counter]])%26
+                        new_ord = (dictionary[chr]+dictionary[code[code_counter]])%26
                         new_chr = inverted_dictionary[new_ord]
                     else: 
-                        new_ord = (dictionary[chr]-dictionary[code[counter]])%26
+                        new_ord = (dictionary[chr]-dictionary[code[code_counter]])%26
                         new_chr = inverted_dictionary[new_ord]
-                    if counter >= len(code)-1:
-                        counter = 0
+                    if code_counter >= len(code)-1:                                              # 'code_counter' es una variable que permite iterar sobre cada caracter de la clave elegida, una vez que es agotada, vuelve a repetirse
+                        code_counter = 0
                     else:
-                        counter +=1
+                        code_counter +=1
                 else:
                     new_chr = chr
                 new_line += new_chr
+                
             f.write(new_line)
 
-def try_path(prompt:str):
+def try_path(prompt:str) -> list:
     '''
-    Verifica que el archivo exista y, en caso de existir, devuelve las lineas del archivo.
-    Argumentos:
-    Prompt -- Mensaje de input
+    Verifica que el archivo exista y, en caso de existir, lo lee y devuelve una lista de listas que corresponden a
+    las lineas del archivo 
+    
+    Parámetros de entrada
+    ---------------------
+    prompt: str
+    Mensaje mostrado al usuario para que realice la entrada
+
+    Parámetros de salida
+    --------------------
+    data: list
+    Lista de listas, con cadenas de texto correspondientes a las líneas del archivo abierto
     '''
     path = input(prompt)
     try:
@@ -57,25 +77,43 @@ def try_path(prompt:str):
         return data
     except FileNotFoundError:
         print ('...\nNo se pudo abrir el archivo')
-        try_path(prompt)
+        try_path(prompt)                                    # En caso de no poder abrir el archivo, se volverá a pedir un ingreso hasta que este se haga efectivo
 
-def try_code (prompt: str):
+def try_code (prompt: str) -> str:
     ''' 
-    Corrobora que la clave ingresada sea válida (unicamente letras del alfabeto inglés)
-    Prompt -- Mensaje de input
+    Corrobora que la clave ingresada, convertida a letras minúsculas, sea válida (deben ser unicamente letras del alfabeto inglés)
+
+    Parámetros de entrada
+    ---------------------
+    prompt: str
+    Mensaje mostrado al usuario para que realice la entrada
+
+    Parámetros de salida
+    --------------------
+    code: str
+    Clave ingresada
     '''
     code = input(prompt)
     code = code.lower()
     for chr in code:
         if chr not in ABC:
             print('...\nLa clave solo puede contener letras del alfabeto inglés') 
-            try_code(prompt)
+            try_code(prompt)                                                            # En caso de una entrada inesperada, se pedirá otra clave hasta que se cumplan las condiciones
     return code
 
-def try_archive (prompt:str):
+def try_archive (prompt:str) -> str:
     '''
-    Impone las condiciones del nombre del archivo de salida.
-    Prompt -- Mensaje de input
+    Impone las condiciones para el nombre del archivo a generar.
+
+    Parámetros de entrada
+    ---------------------
+    prompt: str
+    Mensaje mostrado al usuario para que realice la entrada
+
+    Parámetros de salida
+    --------------------
+    archive: str
+    Nombre para el archivo a generar
     '''
     archive = input(prompt)
     invalid_chr = '\/:*?"<>|'                
@@ -84,7 +122,7 @@ def try_archive (prompt:str):
                 return archive
         else:
             print('...\nEl nombre para el archivo es inválido')  
-            try_archive(prompt)
+            try_archive(prompt)                                             # En caso de que no sea un nombre válido para un archivo, se pedirá uno nuevo hasta que se cumplan las condiciones
 
 if __name__ == "__main__":
     main()
